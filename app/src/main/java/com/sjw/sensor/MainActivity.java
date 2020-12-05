c0ad8fb919d3169a44b100bf21d8c28c9df75a54
sjw <sunjiangwei@qq.com>
2020-12-05 10:08:47 +0800

    地图功能完成

--- app/src/main/java/com/sjw/sensor/MainActivity.java
+++ app/src/main/java/com/sjw/sensor/MainActivity.java
@@ -351,6 +351,38 @@ public class MainActivity extends Activity implements SensorEventListener, Locat
         // 缩放级别（zoom）：地图缩放级别范围为【4-20级】，值越大地图越详细
         aMap.moveCamera(CameraUpdateFactory.zoomTo(16));
 
+        //设置显示定位按钮 并且可以点击
+        UiSettings settings = aMap.getUiSettings();
+        //设置定位监听
+        aMap.setLocationSource(this);
+        // 是否显示定位按钮
+        settings.setMyLocationButtonEnabled(true);
+        //初始化定位
+        mLocationClient = new AMapLocationClient(getApplicationContext());
+        //设置定位回调监听
+        mLocationClient.setLocationListener(this);
+        //初始化定位参数
+        mLocationOption = new AMapLocationClientOption();
+        //设置定位模式为高精度模式，Battery_Saving为低功耗模式，Device_Sensors是仅设备模式
+        mLocationOption.setLocationMode(AMapLocationClientOption.AMapLocationMode.Hight_Accuracy);
+        //设置是否返回地址信息（默认返回地址信息）
+        mLocationOption.setNeedAddress(true);
+        //设置是否只定位一次,默认为false
+        mLocationOption.setOnceLocation(false);
+        //获取最近3s内精度最高的一次定位结果：必须开，否则无法更新位置信息
+        //设置setOnceLocationLatest(boolean b)接口为true，启动定位时SDK会返回最近3s内精度最高的一次定位结果。如果设置其为true，setOnceLocation(boolean b)接口也会被设置为true，反之不会，默认为false。
+        mLocationOption.setOnceLocationLatest(true);
+        //设置是否强制刷新WIFI，默认为强制刷新
+        mLocationOption.setWifiActiveScan(true);
+        //设置是否允许模拟位置,默认为false，不允许模拟位置
+        mLocationOption.setMockEnable(false);
+        //设置定位间隔,单位毫秒,默认为2000ms, 设置成1s更新一次
+        mLocationOption.setInterval(1000);
+        //给定位客户端对象设置定位参数
+        mLocationClient.setLocationOption(mLocationOption);
+        //启动定位
+        mLocationClient.startLocation();
+
         //定位的小图标 默认是蓝点
         MyLocationStyle myLocationStyle = new MyLocationStyle();
         myLocationStyle.interval(10);
@@ -370,7 +402,7 @@ public class MainActivity extends Activity implements SensorEventListener, Locat
         // 绘制一个大地曲线
         aMap.addPolyline((new PolylineOptions())
                 .add(oldData, newData)
-                .geodesic(true).color(Color.GREEN));
+                .geodesic(true).color(Color.BLUE));
 
     }
 
